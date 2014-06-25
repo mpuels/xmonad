c4671663eb3173bdaf94d43a0dbeb7739e2cdaf4
Marc Puels <mpuels@web.de>
2014-06-25 22:09:12 +0200

    Initial commit.

---
+++ xmonad.hs
@@ -0,0 +1,18 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig
+import System.IO
+
+main = do
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+	, modMask = mod4Mask  -- Rebind Mod to the Windows key
+        }
+	`additionalKeysP`
+	[ ("<XF86AudioRaiseVolume>", spawn "changeVolume.bash +")
+        , ("<XF86AudioLowerVolume>", spawn "changeVolume.bash -")
+        , ("<XF86AudioMute>", spawn "amixer sset Master toggle")
+	]
