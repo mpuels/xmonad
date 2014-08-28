f6668bfea1a9e8b646b6a8eb0df43df944d8ab69
Marc Püls <mpuels@gmail.com>
2014-08-28 21:23:56 +0200

    Add xmobarPP to xmonad.hs.

--- xmonad.hs
+++ xmonad.hs
@@ -9,6 +9,10 @@ main = do
     xmonad $ defaultConfig
         { manageHook = manageDocks <+> manageHook defaultConfig
         , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
 	, modMask = mod4Mask  -- Rebind Mod to the Windows key
         }
 	`additionalKeysP`
